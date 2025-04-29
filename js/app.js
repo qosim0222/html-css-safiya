e60336574ed41f92c3ee5ac97ad4a4cd3c4d72aa
qosim0222 <qosimxushvaxtov78@gmail.com>
2025-04-29 07:15:07 +0500

    add

--- js/app.js
+++ js/app.js
@@ -1,9 +1,17 @@
 const headerEl = document.querySelector(".header")
+const  backTop = document.querySelector(".back__top")
 
 window.addEventListener("scroll", ()=>{
+    let scrollPoint = document.documentElement.scrollTop
     if(document.documentElement.scrollTop > 0){
         headerEl.classList.add("shrink")
     }else{
         headerEl.classList.remove("shrink")
     }
+if (scrollPoint > 200) {
+    backTop.style.transform = "scale(1)"
+}else{
+    backTop.style.transform = "scale(0)"
+}
+
 })
